fd24a639ace204b7b960b4b23140752d610a0dc6
abhishek choudhury <mail2abhic@gmail.com>
2016-07-18 17:22:52 +0530

    updated the code with comments and underscore

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 var express = require("express");
+
+// body parser is required for parsing the json data from POST request's body.
+var bodyParser = require("body-parser");
+
+// The convension for declaring variable for underscore is to use '_'.
+// It is required for a set of utilities and validation purposes.
+// It is used as a middleware with Express using app.use()
+var _ = require("underscore");
 var app=express();
+
+// process.env.PORT is the port number set by Heroku..for local server the 9000 port would be used
+// as process.env.PORT is not supplied in the local env
 var port=process.env.PORT||9000;
 
-var todos = [
-	{
-		id:1,
-		description:'Have the lunch by 2PM',
-		completed:false
-	},
-	{
-		id:2,
-		description:'Wake up at 7AM',
-		completed:true
-	},
-	{
-		id:3,
-		description:'Start to home at 5:30PM',
-		completed:true
-	}
+var nextTodoId =1;
+var todos = [];
 
-];
-
+// using bodyparser as a middleware
+app.use(bodyParser.json());
 middleware=require("./middleware.js");
 
 /*app.get("/",
@@ -30,21 +27,21 @@ middleware=require("./middleware.js");
 	}
 );*/
 
-//this is an  applicaiton level middleware
+
+
+//these are applicaiton level middlewares
 //app.use(middleware.requireAuthentication);
 //app.use(middleware.logger);
 
 
-// the following is an example of path specific logger, which is used as a second argument of the route difinition
-app.get("/about",middleware.pathSpecificLogger,
-	function(req,resp)
+// the following is an example of path specific middleware(pathSpecificLogger), which is used as a second argument of the route difinition
+app.get("/about",middleware.pathSpecificLogger,function(req,resp)
 	{
 		resp.send("About items are listed here");
 	}
 );
 
 //GET all todo items
-
 app.get("/todos",
 	function(req,resp)
 	{
@@ -59,13 +56,19 @@ app.get("/todos/:id",
 	{
 		var todoId = parseInt(req.params.id,10);
 		var matchedTodo;
-		todos.forEach(function(todo)
+		
+		//commented for the underscore's usage of _.findWhere() 
+		/*todos.forEach(function(todo)
 		{
 			if(todoId===todo.id)
 			{
 				matchedTodo=todo;
 			}
-		});
+		});*/
+
+
+		// using underscore for finding the element
+		matchedTodo = _.findWhere(todos,{id:todoId});
 
 		if(matchedTodo)
 		{
@@ -79,8 +82,27 @@ app.get("/todos/:id",
 	}
 );
 
+//POST create a new todo item
+app.post("/todos",
+	function(req,resp)
+	{
+		// using body-parser provides the feature of req.body to be used
+		var body = req.body;
+		
+		// adding id with the JSON sent by user/client
+		body.id=nextTodoId++;
+		
+		// add new todo in todos array		
+		todos.push(body);
+		console.log(body);
+		resp.json(body);
+	}
+);
 
-//if you dont provide any route for the "/"  this would get autometically invoked
+
+
+
+//if you dont provide any route for the "/" , this would get autometically invoked
 app.use(express.static(__dirname+"/public"));
 
 app.listen(port,function(error,success){
