f8e40b22a0a25b757d284c8beaaaa41eb5e85788
abhishek choudhury <mail2abhic@gmail.com>
2016-07-18 14:02:42 +0530

    added todo items

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ var express = require("express");
 var app=express();
 var port=process.env.PORT||9000;
 
+var todos = [
+	{
+		id:1,
+		description:'Have the lunch by 2PM',
+		completed:false
+	},
+	{
+		id:2,
+		description:'Wake up at 7AM',
+		completed:true
+	},
+	{
+		id:3,
+		description:'Start to home at 5:30PM',
+		completed:true
+	}
+
+];
+
 middleware=require("./middleware.js");
 
 /*app.get("/",
@@ -12,8 +31,8 @@ middleware=require("./middleware.js");
 );*/
 
 //this is an  applicaiton level middleware
-app.use(middleware.requireAuthentication);
-app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+//app.use(middleware.logger);
 
 
 // the following is an example of path specific logger, which is used as a second argument of the route difinition
@@ -21,6 +40,42 @@ app.get("/about",middleware.pathSpecificLogger,
 	function(req,resp)
 	{
 		resp.send("About items are listed here");
+	}
+);
+
+//GET all todo items
+
+app.get("/todos",
+	function(req,resp)
+	{
+		// instead of sending JSON.stringify() use the readymade function of resp.json()
+		resp.json(todos);
+	}
+);
+
+//GET an individual todo with ID=:id
+app.get("/todos/:id",
+	function(req,resp)
+	{
+		var todoId = parseInt(req.params.id,10);
+		var matchedTodo;
+		todos.forEach(function(todo)
+		{
+			if(todoId===todo.id)
+			{
+				matchedTodo=todo;
+			}
+		});
+
+		if(matchedTodo)
+		{
+			resp.json(matchedTodo);
+		}
+		else
+		{
+			console.log("Reqeust:"+ new Date().toString()+" => "+req.method +" "+ req.originalUrl+" could not be served the todo item with id "+todoId +" does not exist!!");
+			resp.status(404).send();
+		}
 	}
 );
 
